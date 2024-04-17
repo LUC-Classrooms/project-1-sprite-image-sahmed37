3f0e2e35019ee421b9f6f62bf1d9a80f5a64ee30
sahmed37 <156940147+sahmed37@users.noreply.github.com>
2024-04-17 18:56:50 +0000

    fixed final

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ var ySpeed, xSpeed
 function setup(){
   // this function will run once
   createCanvas(600, 400); // create a 600x400 pixel drawing canvas
-x = width / 5
-y = height / 2
-xSpeed = random(1, -1)
-ySpeed = random(1, -1)
+  x = width / 2
+  y = height / 2
+  xSpeed = random(1, -1)
+  ySpeed = random(1, -1)
 }
 
-function draw() {
+function drawOld() {
   background(100,100);
   push();
   translate(x, y);
@@ -29,63 +29,6 @@ function draw() {
  line(x,y,x-10,y-35)
  line(x,y,x+20,y-30)
   ellipse(x, y, 50 ,50)
-fill("green")
-ellipse(x-50,y,50)
-ellipse(x-100, y, 50)
-ellipse(x-150, y, 50)
-ellipse(x-200, y, 50)
-strokeWeight(5)
-point(x-10,y-5)
-point(x+10,y+5)
-}
-
-
-  
-  // this function runs again and again (60x per second)
-  x = x + xSpeed;
-  y += ySpeed;
-
-  if(x > width || x < 0){
-  xSpeed = xSpeed * -1;
-  }
-  if(y > height || y < 0){
-    ySpeed = ySpeed *-1
-  }
-  
-  
-  // add your image drawing code here
-  
-
-/* 
-  Use the following if()...else() structure to incorporate mouse click control of your animation
-*/
-function draw() {
-  if(mouseIsPressed ===true){
-    
-    background(0, 200, 250)
-    line(x-25, y-30, x, y)
-    line(x+25, y-30, x, y)
-    ellipse(x-35, y+45, 50, 55)
-    ellipse(x-30, y+90, 60, 55)
-    ellipse(x+35, y+45, 50, 55)
-    ellipse(x+30, y+90, 60, 55)
-    ellipse(x, y+50, 40, 150)
-    fill('#fae')
-    strokeWeight(3)
-    point(x-5, y-10)
-    point(x+5, y-10);
-    rectMode(CENTER)
-   
-  } else{
-     background(100,100);
-    push();
-    translate(x, y);
-    pop();
-    background(100,100);
-     strokeWeight(3)
-   line(x,y,x-10,y-35)
-   line(x,y,x+20,y-30)
-    ellipse(x, y, 50 ,50)
   fill("green")
   ellipse(x-50,y,50)
   ellipse(x-100, y, 50)
@@ -94,7 +37,66 @@ function draw() {
   strokeWeight(5)
   point(x-10,y-5)
   point(x+10,y+5)
+  pop();
+}// end of draw
+
+
+  
+  // this function runs again and again (60x per second)
+ 
+  
+  // add your image drawing code here
+  
+
+/* 
+  Use the following if()...else() structure to incorporate mouse click control of your animation
+*/
+function draw() {
+  x = x + xSpeed;
+  y += ySpeed;
+
+  if(x > width || x < 0){
+    xSpeed = xSpeed * -1;
   }
+  if(y > height || y < 0){
+    ySpeed = ySpeed *-1
+  }
+  push()
+  translate(x,y);
+  if(mouseIsPressed ===true){
+    background(0, 200, 250)
+    fill('#fae')
+    line(-25, -30, 0, 0)
+    line(25, -30, 0, 0)
+    ellipse(-35, 45, 50, 55)
+    ellipse(-30, 90, 60, 55)
+    ellipse(35, 45, 50, 55)
+    ellipse(30, 90, 60, 55)
+    ellipse(0, 50, 40, 150)
+  
+    strokeWeight(3)
+    point(-5, -10)
+    point(5, -10);
+    rectMode(CENTER);
+   
+  } else{
+    background(100,100);
+
+    background(100,100);
+    strokeWeight(3)
+    line(0,0,-10,-35)
+    line(0,0,20,-30)
+    fill("green")
+    ellipse(0, 0, 50 ,50)
+    ellipse(-50,0,50)
+    ellipse(-100, 0, 50)
+    ellipse(-150, 0, 50)
+    ellipse(-200, 0, 50)
+    strokeWeight(5)
+    point(-10,-5)
+    point(10,5)
+    }
+    pop();
   }
     // do something here if the mouse is NOT pressed
   
